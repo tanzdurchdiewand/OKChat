eb8756afd6b6c34a42bb79228e7ec19e2b6cc38c
Noah <81570626+tanzdurchdiewand@users.noreply.github.com>
2024-09-05 12:01:03 +0200

    chore: Update AI assistant name and description

---
+++ src/features/chat-page/chat-services/chat-api/executeAPICall.ts
@@ -0,0 +1,64 @@
+import axios, { AxiosResponse } from "axios";
+
+export async function executeDataAPICall(
+  req: any,
+  args: { prompt: string },
+  threadId: string,
+  userMessage: string,
+  signal: AbortSignal
+) {
+  console.log("executeDataAPICall called with prompt:", args.prompt);
+
+  if (!args.prompt) {
+    return "No prompt provided";
+  }
+
+  let response: AxiosResponse;
+
+  try {
+    response = await axios.get(
+      "https://app-okp-okcapacity-service-p.azurewebsites.net/api/employees",
+      {
+        headers: {
+          accept: "text/plain",
+          Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktRMnRBY3JFN2xCYVZWR0JtYzVGb2JnZEpvNCIsImtpZCI6IktRMnRBY3JFN2xCYVZWR0JtYzVGb2JnZEpvNCJ9.eyJhdWQiOiJhcGk6Ly83NzY2NzczYS00MGNlLTRhY2EtOGIzMC04ZGZmYzM2Y2NiYzAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9kMGFkNWM3My0wMmVmLTQ0M2MtYjA4Ny1lMjFiODk1M2UwYWMvIiwiaWF0IjoxNzI0OTM1ODczLCJuYmYiOjE3MjQ5MzU4NzMsImV4cCI6MTcyNDk0MDM2NywiYWNyIjoiMSIsImFpbyI6IkFWUUFxLzhYQUFBQVVNaEsxU2w5ZzBmaEQzRW0vRWYwejlzdVpobVQxa0tKZHRndk1wVGoxN3FYTzh2b0FuOUtHdEphNlM5bVduMlNLeEhaa1BBN05uZkFuemFJTCsxSjhsNlFKYVNPOFJXSko5K1JiM2RuQlRZPSIsImFtciI6WyJwd2QiLCJtZmEiXSwiYXBwaWQiOiI3NzY2NzczYS00MGNlLTRhY2EtOGIzMC04ZGZmYzM2Y2NiYzAiLCJhcHBpZGFjciI6IjAiLCJmYW1pbHlfbmFtZSI6IlN0b2NrIiwiZ2l2ZW5fbmFtZSI6Ik5vYWgiLCJpcGFkZHIiOiIxMDkuMTA5LjIwNi4yMjEiLCJuYW1lIjoiTm9haCBTdG9jayIsIm9pZCI6IjJkZThiMWFkLWRjYWYtNDlmOC04MDAxLTA5ZTZmOWY5MjUxOCIsInJoIjoiMC5BUkFBYzF5dDBPOENQRVN3aC1JYmlWUGdyRHAzWm5mT1FNcEtpekNOXzhOc3k4Q1hBQTAuIiwicm9sZXMiOlsiQXBwLlJlYWQiXSwic2NwIjoiYWNjZXNzX2FzX3VzZXIiLCJzdWIiOiJGMUEyVjFxZG9tU18zY3ViY29GYm9PLWIydGdoRmQxdVlQZHlpNXBfLUY4IiwidGlkIjoiZDBhZDVjNzMtMDJlZi00NDNjLWIwODctZTIxYjg5NTNlMGFjIiwidW5pcXVlX25hbWUiOiJuc3RAb2JqZWt0a3VsdHVyLmRlIiwidXBuIjoibnN0QG9iamVrdGt1bHR1ci5kZSIsInV0aSI6InRFckxMam5PMGtLaXh2dDdmLThRQUEiLCJ2ZXIiOiIxLjAifQ.OEXUc_1Q4uX9J_TItNUupebKq0fGsQVunutUjAT6EV7hjVOwGY__slxj37I8RxM9SX0gAQVShEXnlr2AJelk_0ny0pr7dc9HEM_dbtTS9pobMC2CKzRTifE_w_egiayJwz-hHtUfO_jUMG54pKG-CaIIPgYXXKCd_l8ty6-rDRWQ_pibiZdWohPP9wfucgYI1Cd1av5mhtDYfaSPRkA8Nboua6kHhAoVugCuOFIPW_iGYg-QJjXqHI0q0Py8yvL0wwNVCng7xVCc0bDt4GOEgkaT8OgG0p59noKdiK5AIidU0T3pCMNfrGHb1c6Z2B-zrCfkwrWFliGyH7_jUylyiQ`,
+        },
+        timeout: 5000,
+      }
+    );
+  } catch (error) {
+    console.error("🔴 error:\n", error);
+    return {
+      error:
+        "There was an error fetching the data: " +
+        error +
+        "Return this message to the user and halt execution.",
+    };
+  }
+
+  console.log("response.data", response.data);
+  const cutresponse = response.data.map(
+    (item: { id: any; name: any; email: any }) => ({
+      id: item.id,
+      name: item.name,
+      email: item.email,
+    })
+  );
+
+  console.log("cutresponse", cutresponse);
+  try {
+    const updated_response = {
+      data: null,
+    };
+
+    return updated_response;
+  } catch (error) {
+    console.error("🔴 error:\n", error);
+    return {
+      error:
+        "There was an error processing the data: " +
+        error +
+        "Return this message to the user and halt execution.",
+    };
+  }
+}
--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,10 +1,11 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "OK GPT";
+export const AI_DESCRIPTION = "OK GPT is a friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
 
 You have access to the following functions:
-1. create_img: You must only use the function create_img if the user asks you to create an image.`;
+1. create_img: You must only use the function create_img if the user asks you to create an image.
+2. create_sql_query: This function must always be called when the user asks for information related to the company, company data, or database-related tasks. Use this function to generate and execute an SQL query based on the user's input in natural language. Do not call this function directly with a predefined SQL statement, but only by interpreting the user's request.`;
 
 export const NEW_CHAT_NAME = "New chat";
